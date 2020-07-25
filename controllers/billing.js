eae55e3f1413fd958cdaa1b7d45fb4a07248e309
asharyapandey <asharyap7@gmail.com>
2020-07-25 09:12:09 +0545

    added route to fit the scanner

--- controllers/billing.js
+++ controllers/billing.js
@@ -1,12 +1,12 @@
-const Device = require('../models/Device');
-const Product = require('../models/Product');
-const Bill = require('../models/Bill');
-const socket = require('../socket');
+const Device = require("../models/Device");
+const Product = require("../models/Product");
+const Bill = require("../models/Bill");
+const socket = require("../socket");
 
 exports.getDevicePage = async (req, res) => {
 	try {
 		const devices = await Device.find();
-		res.render('billing', { devices });
+		res.render("billing", { devices });
 	} catch (errror) {
 		console.log(errror);
 	}
@@ -16,8 +16,8 @@ exports.getBilling = async (req, res) => {
 	try {
 		const deviceID = req.params.deviceID;
 		const device = await Device.findById(deviceID);
-		const data = await device.cart.populate('items.product').execPopulate();
-		res.render('device-billing', { data: data.items });
+		const data = await device.cart.populate("items.product").execPopulate();
+		res.render("device-billing", { data: data.items });
 	} catch (error) {
 		console.log(error);
 	}
@@ -25,17 +25,24 @@ exports.getBilling = async (req, res) => {
 
 exports.getAddToDevice = async (req, res, next) => {
 	const products = await Product.find();
-	res.render('add-to-device', {
+	res.render("add-to-device", {
 		products,
 	});
 };
 
 exports.postAddToDevice = async (req, res, next) => {
-	const productID = req.body.productID;
+	let barcode = parseInt(req.body.barcode);
 	const deviceID = req.body.deviceID;
-	const device = await Device.findById(deviceID);
-	await device.addToCart(productID);
-	res.redirect('/billing/device-dashboard/add-to-device');
+	try {
+		const device = await Device.findById(deviceID);
+		const product = await Product.find({ barcode: barcode });
+		console.log(product[0]._id);
+		await device.addToCart(product[0]._id);
+		res.send("done");
+	} catch (err) {
+		console.log(err);
+		res.send("notdone");
+	}
 };
 
 exports.getCheckout = async (req, res, next) => {
@@ -50,7 +57,7 @@ exports.postCheckout = async (req, res, next) => {
 	const paid = req.body.paid;
 	const deviceID = req.body.deviceID;
 	const device = await Device.findById(deviceID);
-	const data = await device.cart.populate('items.product').execPopulate();
+	const data = await device.cart.populate("items.product").execPopulate();
 	const updatedData = data.items.map((item) => {
 		return { quantity: item.quantity, product: { ...item.product._doc } };
 	});
@@ -65,7 +72,7 @@ exports.postCheckout = async (req, res, next) => {
 			let billID = savedBill._id;
 			device.clearCart();
 			const io = socket.getIO();
-			io.emit('cartCleared', 'The cart is Cleared');
+			io.emit("cartCleared", "The cart is Cleared");
 			res.status(200).json({ billID: billID });
 		}
 	});
