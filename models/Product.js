711bd5bc4c5394f83d1be953087119914d9bef3c
prasanna-adhikari <prasannaadhikari022@gmail.com>
2020-07-28 19:55:32 +0545

    barcode scan code

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const productSchema = mongoose.Schema({
-	name: String,
-	price: Number,
-	qty: Number,
-	category: String,
-
+  name: String,
+  price: Number,
+  qty: Number,
+  category: String,
+  barcode: Number,
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
