39f0b6699764712fdc8357d7d91d47060b87359f
asharyapandey <asharyap7@gmail.com>
2020-08-04 12:56:30 +0545

    flash msg for invalid username and passwords

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,14 +4,19 @@ const Employee = require("../../models/Employee");
 const Router = express.Router();
 
 Router.get("/", (req, res) => {
-	res.render("admin/login");
+	res.render("admin/login", {
+		errorMsg: req.flash("error"),
+	});
 });
 
 Router.post("/login", async (req, res) => {
 	const { username, password } = req.body;
 	try {
 		const user = await Employee.findOne({ username });
-		if (!user) return res.redirect("/");
+		if (!user) {
+			req.flash("error", "Invalid Username or Password.");
+			return res.redirect("/");
+		}
 		if (user.password === password) {
 			req.session.isLoggedIn = true;
 			req.session.user = user;
@@ -19,6 +24,7 @@ Router.post("/login", async (req, res) => {
 			else return res.redirect("/billing/device-dashboard");
 		}
 		// password incorrect
+		req.flash("error", "Invalid Username or Password.");
 		return res.redirect("/");
 	} catch (err) {
 		console.log(err);
